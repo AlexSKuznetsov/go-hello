a1a42ed8883559ac7414680c882ca11078a9428a
Alexandr Kuznetsov <47721260+AlexSKuznetsov@users.noreply.github.com>
2023-11-08 23:11:49 +0200

    variables

--- src/main.go
+++ src/main.go
@@ -8,4 +8,15 @@ import (
 
 func main() {
 	fmt.Println(quote.Go())
+
+	var name string = "Alex"
+	lastname := "Kuznetsov"
+
+	city, hobby := "Vilnius", "Hiking"
+
+	const STATUS = "married"
+
+	fmt.Println(name, lastname)
+	fmt.Println(city, hobby)
+	fmt.Println(STATUS)
 }
