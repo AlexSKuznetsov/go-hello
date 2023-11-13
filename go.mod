2a70d7340d4d8eb228625dcbde6db0243d8a026e
Alexandr Kuznetsov <47721260+AlexSKuznetsov@users.noreply.github.com>
2023-11-13 21:07:36 +0200

    trying chi web server

--- go.mod
+++ go.mod
@@ -2,9 +2,4 @@ module example/src
 
 go 1.21.3
 
-require rsc.io/quote v1.5.2
-
-require (
-	golang.org/x/text v0.0.0-20170915032832-14c0d48ead0c // indirect
-	rsc.io/sampler v1.3.0 // indirect
-)
+require github.com/go-chi/chi/v5 v5.0.10
--- src/main.go
+++ src/main.go
@@ -1,22 +1,63 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
 
-	"rsc.io/quote"
+	"encoding/json"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
+type User struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+	Bio  string `json:"bio"`
+}
+
+var users = []User{
+	{ID: "1", Name: "John Doe", Bio: "This is John Doe"},
+	{ID: "2", Name: "Some other", Bio: "Some other bio"},
+}
+
 func main() {
-	fmt.Println(quote.Go())
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Recoverer)
 
-	var name string = "Alex"
-	lastname := "Kuznetsov"
+	r.Get("/user", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(users)
+	})
 
-	city, hobby := "Vilnius", "Hiking"
+	r.Post("/user", addUser)
 
-	const STATUS = "married"
+	http.ListenAndServe(":3333", r)
+}
 
-	fmt.Println(name, lastname)
-	fmt.Println(city, hobby)
-	fmt.Println(STATUS)
+func addUser(w http.ResponseWriter, r *http.Request) {
+	var user User
+
+	if r.ContentLength == 0 {
+		http.Error(w, "Request body is empty", http.StatusBadRequest)
+		return
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if user.ID == "" || user.Name == "" || user.Bio == "" {
+		http.Error(w, "User fields cannot be empty", http.StatusBadRequest)
+		return
+	}
+
+	users = append(users, user)
+
+	// Return updated user list as JSON
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(users)
 }
